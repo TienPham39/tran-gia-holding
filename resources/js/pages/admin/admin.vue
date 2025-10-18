d06d24e390cd2f41077d003e18b93604fc9d924c
nineZ-dev <kietzenin2020@gmail.com>
2025-10-18 16:56:14 +0700

    update router

---
+++ resources/js/pages/admin/admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="drawer lg:drawer-open font-sans">
+    <input type="checkbox" id="my-drawer" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col">
+      <Navbar @toggle-drawer="toggleDrawer" />
+      <main class="flex-1 p-4 bg-base">
+        <router-view />
+      </main>
+    </div>
+    <Sidebar />
+  </div>
+</template>
+
+<script setup>
+import Navbar from "../../components/admin/Navbar.vue";
+import Sidebar from "../../components/admin/Sidebar.vue";
+
+// chỉ giữ drawer toggle
+const toggleDrawer = () => {
+  const drawer = document.getElementById("my-drawer");
+  if (drawer) {
+    drawer.checked = !drawer.checked;
+  }
+};
+</script>
