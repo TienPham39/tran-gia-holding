cc5c9ce6aa793a3581afbe90801d5015e518ff75
TienPham39 <tienpg39@gmail.com>
2025-10-21 03:00:54 +0700

    Sửa lỗi ở localhost

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,11 @@
 import axios from 'axios';
-window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const token = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
+if (token) {
+  axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
+} else {
+  console.error('⚠️ Không tìm thấy CSRF token trong meta tag');
+}
