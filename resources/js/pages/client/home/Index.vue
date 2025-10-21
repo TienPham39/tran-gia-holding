5bfbb20c2332b84872f913d0391c63e21cb30eee
hwkhanh <khanh0584883823@gmail.com>
2025-10-21 17:59:06 +0700

    Add component slider

--- resources/js/pages/client/home/Index.vue
+++ resources/js/pages/client/home/Index.vue
@@ -1,17 +1,19 @@
 <template>
   <!-- Navbar component -->
   <Navbar />
-  
+
+  <!-- Slider component -->
+  <Slider />
 <!-- Homepage Videos component -->
   <HomepageVideos />
+
+
+
 
 
   <!-- Contact component -->
   <Contact />
 
-  
-  
-  
   <!-- Footer component -->
   <Footer />
 </template>
@@ -19,6 +21,7 @@
 <script setup>
 import { ref } from "vue";
 import Navbar from "../../../components/client/Navbar.vue";
+import Slider from "../../../components/client/Slider.vue";
 import Contact from "../../../components/client/Contact.vue";
 import Footer from "../../../components/client/Footer.vue";
 import HomepageVideos from "../../../components/client/homepage-videos.vue";
