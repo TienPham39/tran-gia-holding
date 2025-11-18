4852aadd00adcef47455cdc6951f4f8fb6abe6b0
trongld <trongld21@gmail.com>
2025-11-18 22:43:53 +0700

    Thêm UI trang tin tức

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,24 @@
 <template>
-  <router-view>
-    
-  </router-view>
+  <component :is="layoutComponent">
+    <component :is="page.component" />
+  </component>
 </template>
+
+<script setup>
+import { usePage, router } from '@inertiajs/vue3'
+import { computed } from 'vue'
+import ClientLayout from './layouts/client.vue'
+import AdminLayout from './layouts/admin.vue'
+
+const layouts = {
+  client: ClientLayout,
+  admin: AdminLayout,
+}
+
+const page = usePage()
+
+const layoutComponent = computed(() => {
+  const layoutName = page.props.layout || page.component?.layout
+  return layouts[layoutName] || layouts.client
+})
+</script>
