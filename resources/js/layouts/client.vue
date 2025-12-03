66024d764fd39bb421a9224493136ed28062773c
TienPham39 <tienpg39@gmail.com>
2025-12-03 17:55:47 +0700

    Thêm popup liên hệ

--- resources/js/layouts/client.vue
+++ resources/js/layouts/client.vue
@@ -7,10 +7,13 @@
     </main>
 
     <Footer />
+
+    <FloatingContact />
   </div>
 </template>
 
 <script setup>
 import Navbar from "../components/client/Navbar.vue";
 import Footer from "../components/client/Footer.vue";
+import FloatingContact from "../components/client/FloatingContact.vue";
 </script>
